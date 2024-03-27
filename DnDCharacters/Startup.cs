818125ef1b3473dafc569949993c10bb90298457
Hesselbacher, Bryce <bhesselbacher@madisoncollege.edu>
2024-03-27 14:45:34 -0500

    Initial commit.

---
+++ DnDCharacters/Startup.cs
@@ -0,0 +1,24 @@
+﻿using DnDCharacters.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnDCharacters
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+    }
+}
