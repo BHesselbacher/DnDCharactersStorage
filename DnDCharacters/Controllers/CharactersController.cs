4d457e2f2b6d0f3692a89d528426815297301520
Hesselbacher, Bryce <bhesselbacher@madisoncollege.edu>
2024-04-15 14:18:17 -0500

    Deleted DnDCharactersEntityFrameworkCoreDbContext.cs.

--- DnDCharacters/Controllers/CharactersController.cs
+++ DnDCharacters/Controllers/CharactersController.cs
@@ -1,12 +1,4 @@
-﻿using DnDCharacters.Database;
-using DnDCharacters.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DnDCharacters.Controllers
+﻿namespace DnDCharacters.Controllers
 {
     public class CharactersController : Controller
     {
@@ -20,9 +12,7 @@ namespace DnDCharacters.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            List<Character> characters = _characterRepository.GetAllCharacters();
-
-            return View(characters);
+            return View();
         }
 
         [HttpGet]
@@ -34,13 +24,6 @@ namespace DnDCharacters.Controllers
         [HttpPost]
         public IActionResult Create(Character character)
         {
-            if (ModelState.IsValid == true)
-            {
-                _characterRepository.AddCharacter(character);
-
-                return RedirectToAction("Index");
-            }
-
             return View(character);
         }
     }
--- DnDCharacters/GlobalUsings.cs
+++ DnDCharacters/GlobalUsings.cs
@@ -1,4 +1,5 @@
 ﻿global using DnDCharacters.Data;
 global using DnDCharacters.Models;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using System.ComponentModel.DataAnnotations;
